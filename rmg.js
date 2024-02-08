99d038bbd0105e25e5446760d166412b4de1c4e6
Rishabh <rishabhg0707@gmail.com>
2024-02-08 10:09:56 +0000

    replace with react code

--- rmg.js
+++
@@ -1,9 +0,0 @@
-const messages = {
-  first: ["Hi", "Hello", "Namaste"],
-  second: ["Rohit!", "Neha!", "Ayla!", "Abhi!"],
-  third: ["How are you?", "Aap kaise ho?", "Kya haal hai?"]
-}
-
-console.log("A good way to greet someone in India: " + messages.first[Math.floor(Math.random() * messages.first.length)] + " " + messages.second[Math.floor(Math.random() * messages.second.length)] + " " + messages.third[Math.floor(Math.random() * messages.third.length)]);
-
-document.getElementById('get-greeting').addEventListener('click', () => { document.getElementById('random-greeting').innerText = messages.first[Math.floor(Math.random() * messages.first.length)] + " " + messages.second[Math.floor(Math.random() * messages.second.length)] + " " + messages.third[Math.floor(Math.random() * messages.third.length)] });
